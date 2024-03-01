6ed503c5108869ee372509f406c927ff5a108e82
SantiRamoli <sanolydani@gmail.com>
2024-03-01 12:20:30 +0000

    Files updated.

--- Sprint1/script.js
+++ Sprint1/script.js
@@ -1,10 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-  cargarComponente("navbar-container", "/componentes/navbar.html");
-  cargarComponente("login-panel", "/componentes/login_panel.html");
-  cargarComponente("footer", "/componentes/footer.html");
-  cargarComponente("Authenticity", "/componentes/Authenticity.html");
-});
-
+/*
 function cargarComponente(id, url) {
   fetch(url)
       .then(response => response.text())
@@ -13,3 +7,27 @@ function cargarComponente(id, url) {
       })
       .catch(error => console.error('Error al cargar el componente:', error));
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  cargarComponente("navbar-container", "/componentes/navbar.html");
+  //cargarComponente("login-panel", "/componentes/login_panel.html");
+  cargarComponente("footer", "/componentes/footer.html");
+  //cargarComponente("Authenticity", "/componentes/Authenticity.html");
+  cargarComponente("Home", "/componentes/Home.html");
+});
+
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+  // Obtener el contenido del template
+  const template = document.getElementById('Home');
+  const templateContent = template.content.cloneNode(true);
+
+  // Agregar el contenido clonado al contenedor en el index.html
+  const container = document.getElementById('Home');
+  container.appendChild(templateContent);
+
+  // Ahora puedes manipular el contenido del template según necesites
+  console.log(container)
+});
+
+*/
