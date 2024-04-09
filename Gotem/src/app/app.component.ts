74e01ba2538485a6b47a8377d7234669cbfae772
Santiramoli <sanolydani@gmail.com>
2024-04-10 00:28:10 +0100

    Insert "navbar" into app component.

--- Gotem/src/app/app.component.ts
+++ Gotem/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    standalone: true,
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [NavbarComponent]
 })
 export class AppComponent {
   title = 'Gotem';
