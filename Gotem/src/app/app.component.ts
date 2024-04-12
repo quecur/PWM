6844d32ed4a637c4c22a513ac196244585ae0f20
AdriTN <adriiantalavera@gmail.com>
2024-04-12 11:23:19 +0100

    Home component created

--- Gotem/src/app/app.component.ts
+++ Gotem/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
+  providers: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: [
+    './app.component.css'
+  ]
 })
 export class AppComponent {
-  title = 'Gotem';
+  title = 'GOTEM';
 }
--- Gotem/src/app/app.routes.ts
+++ Gotem/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';  // Asegúrate de ajustar la ruta según sea necesario
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }  // Redirige la ruta raíz a Home
+];
