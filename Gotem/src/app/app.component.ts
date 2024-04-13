60fdc680b302307d8499cbdfee53246f317e4f3e
AdriTN <adriiantalavera@gmail.com>
2024-04-13 11:15:04 +0100

    Home with navbar and footer integrated

--- Gotem/src/app/app.component.ts
+++ Gotem/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,7 +9,7 @@ import { RouterOutlet } from '@angular/router';
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [NavbarComponent, HomeComponent, RouterOutlet]
+    imports: [NavbarComponent,FooterComponent, HomeComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'GOTEM';
--- Gotem/src/app/app.routes.ts
+++ Gotem/src/app/app.routes.ts
@@ -2,6 +2,5 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path: '', loadComponent: ()=>import('./navbar/navbar.component').then(m => m.NavbarComponent)},
   {path: 'home', loadComponent: ()=>import('./home/home.component').then(m => m.HomeComponent)}
 ];
