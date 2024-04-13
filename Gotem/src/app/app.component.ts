906d7a7ab1b87d7eafb505fd498f1ccd46bf5c54
AdriTN <adriiantalavera@gmail.com>
2024-04-13 13:01:44 +0100

    About component created

--- Gotem/src/app/app.component.ts
+++ Gotem/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { NavbarComponent } from "./navbar/navbar.component";
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { AuthenticityComponent } from './authenticity/authenticity.component';
+import { AboutComponent } from './about/about.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,7 +11,7 @@ import { RouterOutlet } from '@angular/router';
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [NavbarComponent,FooterComponent, HomeComponent, AuthenticityComponent, RouterOutlet]
+    imports: [NavbarComponent,FooterComponent, HomeComponent, AuthenticityComponent, AboutComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'GOTEM';
--- Gotem/src/app/app.routes.ts
+++ Gotem/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import { SneakersComponent } from './sneakers/sneakers.component';
 import { AuthenticityComponent } from './authenticity/authenticity.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'sneakers', component: SneakersComponent},
-  {path: 'authenticity', component: AuthenticityComponent}
+  {path: 'authenticity', component: AuthenticityComponent},
+  {path: 'about', component: AboutComponent}
 ];
