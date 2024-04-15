8a18a0c765d7dd8c828adacef2b3c9c3b57f72c2
AdriTN <adriiantalavera@gmail.com>
2024-04-15 11:05:27 +0100

    Cart component created

--- Gotem/src/app/app.routes.ts
+++ Gotem/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import {HomeComponent} from './home/home.component';
 import { SneakersComponent } from './sneakers/sneakers.component';
 import { AuthenticityComponent } from './authenticity/authenticity.component';
 import { AboutComponent } from './about/about.component';
+import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'sneakers', component: SneakersComponent},
   {path: 'authenticity', component: AuthenticityComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'cart', component: CartComponent}
 ];
