b8232c06696a494ba6cfa0214c04a8581ba60964
AdriTN <adriiantalavera@gmail.com>
2024-04-26 11:02:33 +0100

    Login and register components created

--- Gotem/src/app/app.component.ts
+++ Gotem/src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "./navbar/navbar.component";
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { AuthenticityComponent } from './authenticity/authenticity.component';
-import { AboutComponent } from './about/about.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
--- Gotem/src/app/app.routes.ts
+++ Gotem/src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { AuthenticityComponent } from './authenticity/authenticity.component';
 import { AboutComponent } from './about/about.component';
 import { BuyComponent } from './buy/buy.component';
 import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 export const routes: Routes = [
@@ -15,5 +17,7 @@ export const routes: Routes = [
   {path: 'authenticity', component: AuthenticityComponent},
   {path: 'about', component: AboutComponent},
   {path: 'buy', component: BuyComponent},
-  {path: 'cart', component: CartComponent}
+  {path: 'cart', component: CartComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
 ];
