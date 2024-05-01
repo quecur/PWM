16bf72716285cd20cd7a688c91aac46c2a3d1409
Santiramoli <sanolydani@gmail.com>
2024-05-01 01:30:49 +0100

    Avanzando componentes.

--- Gotem/src/app/components/home/home.component.ts
+++ Gotem/src/app/components/home/home.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  
+  scrollDown(pixels: number){
+    window.scrollBy({ top: pixels, behavior: 'smooth' });
+  }
   
 }
